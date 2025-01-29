fb7c929fc081e876426da80ac7b67d0520fdf9ff
44David <david.soane@outlook.com>
2025-01-29 11:46:03 -0600

    Added vscode command

--- src/extension.ts
+++ src/extension.ts
@@ -14,7 +14,7 @@ export async function activate(context: vscode.ExtensionContext) {
 	// The command has been defined in the package.json file
 	// Now provide the implementation of the command with registerCommand
 	// The commandId parameter must match the command field in package.json
-	const disposable = vscode.commands.registerCommand('protea.ollama-models', async () => {
+	const disposable = vscode.commands.registerCommand('protea.protea-search', async () => {
 		
 	
 		const models = fetch("https://ollama-models.zwz.workers.dev/");
